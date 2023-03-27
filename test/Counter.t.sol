0865687313f1d69679301640e1df4b774738788c
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-03-27 10:47:11 +0200

    test 001

---
+++ test/Counter.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
+
+
+contract CounterTest is Test {
+    Counter public Counter
+
+    
+
+
+}
