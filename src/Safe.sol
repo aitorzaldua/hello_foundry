1230d35fcd3aa6f9113c381cd444f014199d8bcf
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-03-30 11:50:50 +0200

    test_Withdraw fuzzing

--- src/Safe.sol
+++ src/Safe.sol
@@ -2,19 +2,9 @@
 pragma solidity ^0.8.9;
 
 contract Safe {
+    receive() external payable {}
 
-receive() external payable {}
-
-function withdraw() external payable {
-    payable(msg.sender).transfer(address(this).balance);
-}
-
-function send() external payable {
-    payable(address(this)).transfer(msg.value);
-}
-
-function sendTo(address _address) external payable {
-    payable(_address).transfer(msg.value);
-}
-
+    function withdraw() external {
+        payable(msg.sender).transfer(address(this).balance);
+    }
 }
--- test/SafeV2.t.sol
+++ test/SafeV2.t.sol
@@ -2,24 +2,17 @@
 pragma solidity 0.8.10;
 
 import "forge-std/Test.sol";
-
 import "../src/Safe.sol";
 
-contract SafeTest is Test {
+contract SafeTestV2 is Test {
     Safe safe;
 
+    // Needed so the test contract itself can receive ether
+    // when withdrawing
     receive() external payable {}
 
     function setUp() public {
         safe = new Safe();
-    }
-
-    function test_Withdraw() public {
-        payable(address(safe)).transfer(1 ether);
-        uint256 preBalance = address(this).balance;
-        safe.withdraw();
-        uint256 postBalance = address(this).balance;
-        assertEq(preBalance + 1 ether, postBalance);
     }
 
     function testFuzz_Withdraw(uint96 amount) public {
@@ -31,4 +24,3 @@ contract SafeTest is Test {
         assertEq(preBalance + amount, postBalance);
     }
 }
-
